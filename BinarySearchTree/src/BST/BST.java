a7b570c4095f3095652f46be75dedce741b2ebc1
pas8 <peterpreneur@gmail.com>
2022-01-24 23:09:12 -0600

    initial

---
+++ BinarySearchTree/src/BST/BST.java
@@ -0,0 +1,15 @@
+package BST;
+
+public class BST<K, V> {
+
+	public void insert(K key, V value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object get(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
